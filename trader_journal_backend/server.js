2649b09bb4fe34c9922db4ad5c64ee8941a7724b
sebsto93 <s.stobiecki93@gmail.com>
2025-02-16 19:07:27 +0100

    Users.js
    Made user schema

--- trader_journal_backend/server.js
+++ trader_journal_backend/server.js
@@ -6,7 +6,10 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI, { 
+  useNewUrlParser: true, 
+  useUnifiedTopology: true
+ })
   .then(() => console.log('Połączono z MongoDB'))
   .catch((err) => console.log('Błąd połączenia z MongoDB:', err));
 
