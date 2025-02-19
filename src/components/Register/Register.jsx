b3ecd7800ab8f2ea63acc9f735e67be67512b979
sebsto93 <s.stobiecki93@gmail.com>
2025-02-19 14:06:26 +0100

    Update Register.jsx

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -7,6 +7,8 @@ const Register = () => {
   const [password, setPassword] = useState('');
   const [error, setError] = useState(null); 
 
+  const navigate = useNavigate();
+
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
